350fc76b7e570c57804a01dff29ee29d39c75488
Nick Olson <naolson.1819@gmail.com>
2025-04-17 16:43:06 -0500

    fixed active nav button

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,23 +1,38 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import {
+  createRootRoute,
+  Link,
+  Outlet,
+  useMatchRoute,
+} from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+
+function NavbarLink({
+  to,
+  children,
+}: {
+  to: string;
+  children: React.ReactNode;
+}) {
+  const matchRoute = useMatchRoute();
+  const isActive = !!matchRoute({ to, fuzzy: false });
+
+  return (
+    <Link
+      to={to}
+      className={`btn btn-soft btn-accent mx-2 ${isActive ? 'btn-active' : ''}`}
+    >
+      {children}
+    </Link>
+  );
+}
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <div className="navbar bg-base-100 shadow-sm flex">
         <div className="flex-1"></div>
-        <Link
-          to="/"
-          className="[&.active]:btn-active btn btn-soft btn-accent mx-2"
-        >
-          Login
-        </Link>
-        <Link
-          to="/about"
-          className="[&.active]:btn-active btn btn-soft btn-accent mx-2"
-        >
-          About
-        </Link>
+        <NavbarLink to="/">Login</NavbarLink>
+        <NavbarLink to="/about">About</NavbarLink>
       </div>
       <Outlet />
       <TanStackRouterDevtools />
