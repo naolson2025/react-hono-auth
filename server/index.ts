7a38d8ac552a104213af75b07e5266c44340e4ba
Nick Olson <naolson.1819@gmail.com>
2025-04-17 08:51:34 -0500

    added tanstack router

--- server/index.ts
+++ server/index.ts
@@ -3,16 +3,7 @@ import { serveStatic } from 'hono/bun';
 
 const app = new Hono();
 
-const route = app
-  .use('/*', serveStatic({ root: './client/dist' }))
-  .get('/api/books', (c) => {
-    return c.json([
-      { id: 1, title: '1984', author: 'George Orwell' },
-      { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-      { id: 3, title: 'The great Gatsby', author: 'F. Scott Fitzgerald' },
-      { id: 4, title: 'The catcher in the Rye', author: 'J.D. Salinger' },
-    ]);
-  });
+const route = app.use('/*', serveStatic({ root: './client/dist' }));
 
 export type AppType = typeof route;
 export default app;
